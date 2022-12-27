0c828592773d5cd152d346386a8ac2e93fffddec
MATTY2401 <bernardo.1939439@studenti.uniroma1.it>
2022-12-27 11:53:31 +0100

    add particles generator

---
+++ Generate/particleRand.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+int const NUMBER_BODY = 4;
+float const dim=100;
+float const mass=100;
+float const vel=100;
+unsigned int const seed=0;
+
+int main(){
+    FILE* file=fopen("particle.txt","w");
+    srand(seed);
+    for(int i=0;i<NUMBER_BODY;i++)
+        fprintf(file,"x=%f, y=%f, m=%f, vx=%f, vy=%f\n",(float)rand()/((float)RAND_MAX/dim),(float)rand()/((float)RAND_MAX/dim),\
+        (float)rand()/((float)RAND_MAX/mass),(float)rand()/((float)RAND_MAX/vel),(float)rand()/((float)RAND_MAX/vel));
+    return 0;
+}
