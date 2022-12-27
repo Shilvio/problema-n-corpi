67c09544491abe4e5aa505243713bc179501e579
MATTY2401 <bernardo.1939439@studenti.uniroma1.it>
2022-12-27 12:03:09 +0100

    add seed line in particle.txt

--- Generate/particleRand.c
+++ Generate/particleRand.c
@@ -10,6 +10,7 @@ unsigned int const seed=0;
 int main(){
     FILE* file=fopen("particle.txt","w");
     srand(seed);
+    fprintf(file,"%d\n",seed);
     for(int i=0;i<NUMBER_BODY;i++)
         fprintf(file,"x=%f, y=%f, m=%f, vx=%f, vy=%f\n",(float)rand()/((float)RAND_MAX/dim),(float)rand()/((float)RAND_MAX/dim),\
         (float)rand()/((float)RAND_MAX/mass),(float)rand()/((float)RAND_MAX/vel),(float)rand()/((float)RAND_MAX/vel));
