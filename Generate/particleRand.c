77ff89b28543bda640f14f8d82f0fb13ebc96b4a
MATTY2401 <bernardo.1939439@studenti.uniroma1.it>
2022-12-27 16:33:36 +0100

    aggiunta calcolo delle forze

--- Generate/particleRand.c
+++ Generate/particleRand.c
@@ -4,7 +4,7 @@
 int const NUMBER_BODY = 4;
 float const dim=100;
 float const mass=100;
-float const vel=100;
+float const force=100;
 unsigned int const seed=0;
 
 int main(){
@@ -13,6 +13,6 @@ int main(){
     fprintf(file,"%d %d\n",seed,NUMBER_BODY);
     for(int i=0;i<NUMBER_BODY;i++)
         fprintf(file,"%f %f %f %f %f\n",(float)rand()/((float)RAND_MAX/dim),(float)rand()/((float)RAND_MAX/dim),\
-        (float)rand()/((float)RAND_MAX/mass),(float)rand()/((float)RAND_MAX/vel),(float)rand()/((float)RAND_MAX/vel));
+        (float)rand()/((float)RAND_MAX/mass),(float)rand()/((float)RAND_MAX/force),(float)rand()/((float)RAND_MAX/force));
     return 0;
 }
--- Single_Thread/Exaustive/Exaustive.c
+++ Single_Thread/Exaustive/Exaustive.c
@@ -2,20 +2,42 @@
 #include <stdlib.h>
 #include <math.h>
 
-int numberBody,seed;
+int numberBody,seed,maxTime;
+char fileInput[]="../../Generate/particle.txt";
+float const G=(float)6.67384E-11;;
 
 typedef struct particle{
     float x;
     float y;
     float mass;
-    float vel_x;
-    float vel_y;
+    float forceX;
+    float forceY;
 }particle;
 
+/**/
+void calculateTotalForce(particle p,particle* p1,int j){
+    for(int i=0; i<numberBody;i++){
+        if (i==j){
+            continue;
+        }
+        float xDiff=p1[j].x-p1[i].x;
+        float yDiff=p1[j].y-p1[i].y;
+        float dist=sqrt(xDiff*xDiff+yDiff*yDiff);
+        float cubeDist=dist*dist*dist;
+        p1[j].forceX-=G*p1[j].mass*p1[i].mass/cubeDist*xDiff;
+        p1[j].forceY-=G*p1[j].mass*p1[i].mass/cubeDist*yDiff;
+    }
+}
 
-void compute(int T){
-    for(int i=0; i<T; i++){
-        printf("ciao\n");
+// da finire
+void compute(int time,particle* p1){
+    for(int i=0; i<time; i++){
+        for(int j=0;j<numberBody;j++){
+            calculateTotalForce(p1[j],p1,j);
+        }
+        for(int j=0;j<numberBody;j++){
+            calculatePositionVel(p1[j]);
+        }
     }
 }
 
@@ -23,14 +45,14 @@ void getInput(FILE* file,particle* p1){
     //mi creo l'arry che dove poi metterò tutte le particelle
     //for numberBody
     for(int i=0;i<numberBody;i++){
-        fscanf(file,"%f%f%f%f%f",&p1[i].x,&p1[i].y,&p1[i].mass,&p1[i].vel_x,&p1[i].vel_y);
-        printf("particle %f, %f, %f, %f, %f\n",p1[i].x,p1[i].y,p1[i].mass,p1[i].vel_x,p1[i].vel_y);
+        fscanf(file,"%f%f%f%f%f",&p1[i].x,&p1[i].y,&p1[i].mass,&p1[i].forceX,&p1[i].forceY);
+        printf("particle %f, %f, %f, %f, %f\n",p1[i].x,p1[i].y,p1[i].mass,p1[i].forceX,p1[i].forceY);
     }
     fclose(file);
 }
 
 FILE* initial(){
-    FILE* file=fopen("../../Generate/particle.txt","r");
+    FILE* file=fopen(fileInput,"r");
     //prendo e stampo il seed
     fscanf(file,"%d",&seed);
     printf("%d\n",seed);
@@ -46,7 +68,7 @@ int main(){
     getInput(file,p1);
     printf("\n");
     for(int i=0;i<numberBody;i++){
-        printf("particle %f, %f, %f, %f, %f\n",p1[i].x,p1[i].y,p1[i].mass,p1[i].vel_x,p1[i].vel_y);
+        printf("particle %f, %f, %f, %f, %f\n",p1[i].x,p1[i].y,p1[i].mass,p1[i].forceX,p1[i].forceY);
     }
-    //compute(T);
+    //compute(maxTime,p1);
 }
