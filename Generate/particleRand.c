f2634acd69a633ee6fc290df2d9b25475617fa3f
MATTY2401 <bernardo.1939439@studenti.uniroma1.it>
2022-12-29 11:52:31 +0100

    immessa possibilità di velocità minima

--- Generate/particleRand.c
+++ Generate/particleRand.c
@@ -4,7 +4,7 @@
 int const NUMBER_BODY = 4;
 float const dim=100;
 float const mass=100;
-float const force=100;
+float const vel=200;
 unsigned int const seed=0;
 
 int main(){
@@ -13,6 +13,6 @@ int main(){
     fprintf(file,"%d %d\n",seed,NUMBER_BODY);
     for(int i=0;i<NUMBER_BODY;i++)
         fprintf(file,"%f %f %f %f %f\n",rand()/(RAND_MAX/dim),rand()/(RAND_MAX/dim),\
-        rand()/(RAND_MAX/mass),rand()/(RAND_MAX/force),rand()/(RAND_MAX/force));
+        rand()/(RAND_MAX/mass),rand()/(RAND_MAX/vel)-vel/2,rand()/(RAND_MAX/vel)-vel/2);
     return 0;
 }
