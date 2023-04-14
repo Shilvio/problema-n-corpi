4a37886c0ab18447db795eea3c03c3072c26b057
Shilvio <gori.1844168@.uniroma1.it>
2023-04-14 11:26:22 +0200

    test ebitenenginV2

--- src/main/main.go
+++ src/main/main.go
@@ -2,25 +2,30 @@ package main
 
 import (
 	"bufio"
+	"image/color"
+
 	"fmt"
+	"log"
+	"math/rand"
 	"os"
+	"os/exec"
 	"strconv"
 	"strings"
-
-	"log"
-	"os/exec"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
 
-// particle dot
+// particle structure
 type particle struct {
 	id   float64
 	xPos float64
 	yPos float64
 	img  *ebiten.Image
 }
+
+// global particle's array
+var data []particle
 
 func loadParticle() []particle {
 	file, err := os.Open("../particles-data/particle.txt")
@@ -44,7 +49,9 @@ func loadParticle() []particle {
 		id, _ := strconv.ParseFloat(tmp[2], 64)
 		xPos, _ := strconv.ParseFloat(tmp[0], 64)
 		yPos, _ := strconv.ParseFloat(tmp[1], 64)
-		dataTmp = append(dataTmp, particle{id, xPos, yPos, ebiten.NewImage(1, 1)})
+		img := ebiten.NewImage(2, 2)
+		img.Fill(color.White)
+		dataTmp = append(dataTmp, particle{id, xPos, yPos, img})
 
 	}
 
@@ -52,11 +59,11 @@ func loadParticle() []particle {
 }
 
 // screen options
-const screenWidth = 512
-const screenHeight = 512
+var screenWidth = 1024
+var screenHeight = 1024
 
 // algorithm calling and particle generation functions
-func generateParticlesCall() {
+func algorithmCall() {
 	fmt.Println("calling c barnes-hut")
 	_, err := exec.Command("../generate-particles/particleRand").Output()
 	if err != nil {
@@ -87,24 +94,45 @@ func init() {
 type Game struct{}
 
 func (g *Game) Update() error {
+
+	for _, elem := range data {
+		fmt.Println(elem)
+	}
+
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
 	ebitenutil.DebugPrint(screen, fmt.Sprintf("FPS: %0.2f", ebiten.ActualFPS()))
+	options := &ebiten.DrawImageOptions{}
+
+	for _, elem := range data {
+
+		img := elem.img
+		options.GeoM.Translate(float64(rand.Intn(512)), float64(rand.Intn(512)))
+		screen.DrawImage(img, options)
+	}
+
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
-	return 512, 512
+	return screenWidth, screenWidth
 }
 
 func main() {
-	/*ebiten.SetWindowSize(screenWidth, screenHeight)
-
+	data := loadParticle()
+	if data == nil {
+		log.Fatal("invalid data")
+		os.Exit(1)
+	}
+	ebiten.SetWindowSize(screenWidth, screenHeight)
 	if err := ebiten.RunGame(&Game{}); err != nil {
 		log.Fatal(err)
-	}*/
+	}
 
-	data := loadParticle()
-	fmt.Println(data)
+	//data := loadParticle()
+	//if data == nil {
+	//log.Fatal("invalid data")
+	//}
+
 }
