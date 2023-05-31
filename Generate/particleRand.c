3a94d32c0d75122368546846d25478e6170454ed
Shilvio <silvioxd12@gmail.com>
2023-05-31 16:38:39 +0200

    Parallelize input reading on MPI implementation

--- Generate/particleRand.c
+++ Generate/particleRand.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-int const NUMBER_BODY = 4;
+int const NUMBER_BODY = 6;
 double const dim = 10000;
 double const mass = 5.000e+06;
 double const vel = 10;
--- MPI/Exaustive/ExaustiveMultiWindows.c
+++ MPI/Exaustive/ExaustiveMultiWindows.c
@@ -1,5 +1,6 @@
 #include <mpi.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 int numberBody, seed, maxTime = 5;
 char fileInput[] = "../../Generate/particle.txt";
@@ -9,6 +10,9 @@ double const deltaTime = 1;
 
 int nProcess;
 int id;
+
+int bodyForProcess;
+int remainBodies;
 
 MPI_Datatype MPIParticle;
 typedef struct particle
@@ -24,33 +28,44 @@ typedef struct particle
 
 void getInput(FILE *file, particle *p1)
 {
-  int bodyForProcess = numberBody / nProcess;
-  int StronziAvanzati = numberBody % nProcess;
 
   // ci spostiamo nel punto giusto del file--------------------------------------------------------------------------------------
 
+  for (int i = 0; i < id * bodyForProcess; i++)
+  {
+    // getline(&buffer,&size,stdin);
+    fscanf(file, "%lf%lf%lf%lf%lf", &p1[0].x, &p1[0].y, &p1[0].mass, &p1[0].velX, &p1[0].velY);
+    // pritnf()
+    //  printf("id:%d ciao\n", id);
+  }
+
+  int pre;
+  int bodyToRead = bodyForProcess;
+
+  if (id < remainBodies)
+  {
+    pre = id;
+    bodyToRead++;
+  }
+  else
+  {
+    pre = remainBodies;
+  }
+
+  for (int i = 0; i < pre; i++)
+  {
+    fscanf(file, "%lf%lf%lf%lf%lf", &p1[0].x, &p1[0].y, &p1[0].mass, &p1[0].velX, &p1[0].velY);
+  }
+
   // prendo i dati per tutti i corpi
-  for (int i = id * nProcess; i < (id + 1) * nProcess; i++)
+  for (int i = 0; i < bodyToRead; i++)
   {
     // prendo i dati dal file
     fscanf(file, "%lf%lf%lf%lf%lf", &p1[i].x, &p1[i].y, &p1[i].mass, &p1[i].velX, &p1[i].velY);
     // imposto le forze iniziali a zero
     p1[i].forceX = 0;
     p1[i].forceY = 0;
-    // printf("particle xPos= %e, yPos= %e, mass= %e, forceX= %e, forceY= %e, velX= %e, velY= %e\n", p1[i].x, p1[i].y, p1[i].mass, p1[i].forceX, p1[i].forceY, p1[i].velX, p1[i].velY);
-  }
-
-  if (id == nProcess - 1)
-  {
-    for (int i = (id + 1) * nProcess; i < numberBody; i++)
-    {
-      // prendo i dati dal file
-      fscanf(file, "%lf%lf%lf%lf%lf", &p1[i].x, &p1[i].y, &p1[i].mass, &p1[i].velX, &p1[i].velY);
-      // imposto le forze iniziali a zero
-      p1[i].forceX = 0;
-      p1[i].forceY = 0;
-      // printf("particle xPos= %e, yPos= %e, mass= %e, forceX= %e, forceY= %e, velX= %e, velY= %e\n", p1[i].x, p1[i].y, p1[i].mass, p1[i].forceX, p1[i].forceY, p1[i].velX, p1[i].velY);
-    }
+    printf("id:%d particle xPos= %e, yPos= %e, mass= %e, forceX= %e, forceY= %e, velX= %e, velY= %e\n", id, p1[i].x, p1[i].y, p1[i].mass, p1[i].forceX, p1[i].forceY, p1[i].velX, p1[i].velY);
   }
   // chiudo il file
   fclose(file);
@@ -62,10 +77,12 @@ FILE *initial()
   FILE *file = fopen(fileInput, "r");
   // prendo il seed
   fscanf(file, "%d", &seed);
-  printf("%d\n", seed);
+  if (id == 0)
+    printf("%d\n", seed);
   // prendo il numero di corpi
   fscanf(file, "%d", &numberBody);
-  printf("%d\n", numberBody);
+  if (id == 0)
+    printf("%d\n", numberBody);
   return file;
 }
 
@@ -85,7 +102,11 @@ int main()
   mpiStart();
   FILE *file = initial();
 
-  particle *p1 = malloc(sizeof(particle) * numberBody);
+  bodyForProcess = numberBody / nProcess;
+  remainBodies = numberBody % nProcess;
 
+  particle *p1 = malloc(sizeof(particle) * (bodyForProcess + 1));
   getInput(file, p1);
+
+  MPI_Finalize();
 }
